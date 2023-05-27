4d099740d1f12778a8839bdfedf3221abbd4ef7d
André Moreira <112489349+andremoreiradev@users.noreply.github.com>
2023-05-27 11:53:58 -0300

    Update pratica.js
    exercício de (array and object)

--- pratica.js
+++ pratica.js
@@ -1,1 +1,30 @@
+const digitaCollege = {
+    cnpj: '45.277.892/0001-00',
+    sedes: [
+        {nome: 'Sul', 
+         endereco: 'WSTC',
+         Ceo: 'Daniel',
+         professores: ['Carlos', 'Alessandro', 'Abrãao'], 
+         alunos: ['André', 'Eduardo', 'Maria', 'José'], 
+        curso: ['FullStack', 'Date Analytcs', 'Designer'], 
+        funcionarios: ['José', 'Maria', 'Antonio']
+                 
+        },
 
+        {nome: 'Aldeota',
+         endereco: 'Santos Dumont',
+        CEO:'Daniel', 
+        professores: ['Carlos', 'Alessandro', 'Abrãao'],
+       alunos: ['André', 'Eduardo', 'Maria', 'José'], 
+       funcionarios:['José', 'Maria', 'Antonio'],
+       curso: 'FullStack'}
+    ]
+}
+
+//Percorrer a lista de alunos da sede Sul
+//digitaCollege.sedes[0].alunos + enter
+//['André', 'Eduardo', 'Maria', 'José']
+
+// Percorrer a lista de professores da sede Aldeota
+//digitaCollege.sedes[1].professores
+//['Carlos', 'Alessandro', 'Abrãao']
